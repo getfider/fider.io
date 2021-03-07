f4aa1ea65c45308621f2054a2f6c58fe7d792143
goenning <me@goenning.net>
2021-03-07 09:48:58 +0000

    add icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,7 @@
-import { Hero, FeatureList, HowItWorks, SEO, Pricing } from '@app/components'
-
 export default function Home(): JSX.Element {
   return (
     <>
-      <SEO
-        title=""
-        description=""
-        imageUrl=""
-      />
-
-      <Hero />
-      <FeatureList />
-      <HowItWorks />
-      <Pricing />
+      <span>Hello World</span>
     </>
   )
 }
