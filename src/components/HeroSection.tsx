5dee3121e8b30bedb9453a6e2fa7aa0ac1608d16
Aikatsui <63988538+Aikatsui@users.noreply.github.com>
2021-03-15 10:58:39 +0700

    Update component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,7 +15,7 @@ export function HeroSection(): JSX.Element {
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
                 <a
-                  href="#"
+                  href="https://getfider.com/#get-started"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                 >
                   Get started
