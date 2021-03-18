2f2c734ae21b445f9cdc2a7dc537884f22411487
goenning <me@goenning.net>
2021-03-18 14:00:43 +0000

    add fider cloud

---
+++ src/components/FAQ.tsx
@@ -0,0 +1,60 @@
+import React, { ReactNode } from 'react'
+
+interface ItemProps {
+  title: string
+  children: ReactNode
+}
+
+function Item(props: ItemProps): JSX.Element {
+  return (
+    <div className="my-6">
+      <div className="font-semibold py-2 px-4 bg-gray-600 rounded-sm">{props.title}</div>
+      <p className="mt-2">{props.children}</p>
+    </div>
+  )
+}
+
+export function FAQ(): JSX.Element {
+  return (
+    <div className="container mx-auto pb-20">
+      <div className="flex flex-wrap justify-center">
+        <div className="w-full ml-auto mr-auto mt-8 lg:mt-16">
+          <h3 className="text-3xl font-semibold leading-normal text-white">Frequently Asked Questions</h3>
+          <div className="text-white mt-4">
+            <Item title="I'm interested on the Cloud option, what's the process to sponsor the project?">
+              <p className="mb-3 leading-7">
+                <a className="text-blue-300" href="https://login.fider.io/signup" target="_blank" rel="noopener noreferrer">
+                  Sign up
+                </a>{' '}
+                for a Fider account and then visit{' '}
+                <a className="text-blue-300" href="https://opencollective.com/fider" target="_blank" rel="noopener noreferrer">
+                  https://opencollective.com/fider
+                </a>{' '}
+                to select one of pre-defined monthly tiers or the pay as you can option. After subscribing, send us an email{' '}
+                <a className="text-blue-300" href="mailto:opensource@fider.io">
+                  opensource@fider.io
+                </a>{' '}
+                with your Fider URL <span className="text-sm">(https://yourname.fider.io)</span> and your OpenCollective profile URL{' '}
+                <span className="text-sm">(https://opencollective.com/yourname)</span> so we can link them.
+              </p>
+            </Item>
+            <Item title="I want to sponsor the project, but I don't want my contribution to become public. What should I do?">
+              You can become an anonymous sponsor by using an OpenCollective incognito mode. You&apos;ll find this option when setting up your contribution on
+              OpenCollective.
+            </Item>
+            <Item title="Is there a free trial period?">
+              Yes. We give everyone 14 days to try the cloud option. After this period, you&apos;ll be asked to moved to a Self-Hosted or sponsor the project.
+            </Item>
+            <Item title="What if I don't want to use Fider Cloud anymore?">
+              You can simply cancel your OpenCollective subscription and delete your Fider account.
+            </Item>
+            <Item title="I want to self host Fider, can I still sponsor the project?">
+              You sure can! We appreciate any code or financial contribution you can give to keep the project alive. The Cloud option is an incentive for
+              individuals and businesses to get something extra in return.
+            </Item>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/GetStartedSection.tsx
+++ src/components/GetStartedSection.tsx
@@ -1,35 +1,96 @@
+import React, { ReactNode } from 'react'
 import GitHubLogo from '@assets/icons/iconmonstr-github.svg'
+import CheckIcon from '@assets/icons/heroicons-check.svg'
 import Link from 'next/link'
+import { FAQ } from './FAQ'
+
+interface ItemProps {
+  children: ReactNode
+}
+
+function Item(props: ItemProps): JSX.Element {
+  return (
+    <li className="flex items-center py-2">
+      <span className={`w-5 h-5 mr-2 inline-flex items-center justify-center bg-green-500 text-white rounded-full flex-shrink-0`}>
+        <CheckIcon className="w-4 h-4" />
+      </span>
+      <span className="text-gray-700 text-lg">{props.children}</span>
+    </li>
+  )
+}
 
 export function GetStartedSection(): JSX.Element {
   return (
-    <section id="get-started">
-      <div className="py-40 bg-gray-700 overflow-hidden">
-        <div className="container mx-auto">
-          <div className="flex flex-wrap justify-center">
-            <div className="w-full md:w-1/2 px-12 md:px-4 ml-auto mr-auto mt-8 lg:mt-16">
-              <h3 className="text-3xl mb-2 font-semibold leading-normal text-white">Get Feedback. Get Fider.</h3>
-              <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-300">Are you ready to try it?</p>
-              <Link href="/docs/hosting-instance">
-                <a
-                  target="_blank"
-                  className="mt-4 inline-block text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-gray-800 active:bg-gray-700 uppercase text-sm shadow hover:shadow-lg"
-                >
-                  Host your own instance
+    <>
+      <section id="get-started">
+        <div className="px-4 md:px-20 xl:px-40 bg-gray-700 overflow-hidden">
+          <div className="container mx-auto">
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full md:w-1/2 px-12 md:px-4 ml-auto mr-auto mt-8 lg:mt-16">
+                <h3 className="text-3xl mb-2 font-semibold leading-normal text-white">Get Feedback. Get Fider.</h3>
+                <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-300">Are you ready to try it?</p>
+              </div>
+              <div className="w-full hidden mt-4 lg:mt-8 md:block md:w-4/12 px-4 mr-auto ml-auto relative">
+                <GitHubLogo className="w-2/4 text-gray-500" />
+              </div>
+            </div>
+          </div>
+          <div className="relative block md:flex items-center">
+            <div className="w-full md:w-1/2 relative z-1 bg-gray-100 rounded shadow-lg overflow-hidden">
+              <div className="text-lg font-medium text-blue-500 uppercase p-8 text-center border-b border-gray-200 tracking-wide">Self Hosted</div>
+              <div className="block sm:flex md:block lg:flex items-center justify-center">
+                <div className="mt-8 sm:m-8 md:m-0 md:mt-8 lg:m-8 text-center">
+                  <div className="inline-flex items-center">
+                    <span className="text-3xl font-medium">$0/mo</span>
+                  </div>
+                  <span className="block text-sm text-gray-600 mt-2">yep, it&apos;s open source</span>
+                </div>
+              </div>
+              <div className="flex justify-center mt-3 px-4 lg:px-8">
+                <ul>
+                  <Item>All features included</Item>
+                  <Item>No setup or maintenance fees</Item>
+                  <Item>You manage the software and updates</Item>
+                </ul>
+              </div>
+
+              <Link href="/docs">
+                <a className="flex items-center justify-center bg-gray-200 hover:bg-gray-300 p-8 text-md font-semibold text-gray-800 uppercase mt-16">
+                  <span>Learn how to install</span>
+                  <span className="font-medium text-gray-700 ml-2">➔</span>
                 </a>
               </Link>
             </div>
-            <div className="w-full hidden mt-4 lg:mt-8 md:block md:w-4/12 px-4 mr-auto ml-auto relative">
-              <GitHubLogo className="w-2/4 text-gray-500" />
+            <div className="w-full md:w-1/2 relative z-0 px-8 md:px-0 md:py-16">
+              <div className="bg-indigo-900 text-white rounded-b md:rounded-b-none md:rounded-r shadow-lg overflow-hidden">
+                <div className="text-lg font-medium uppercase p-8 text-center border-b border-indigo-800 tracking-wide">Cloud</div>
+                <div className="text-center text-sm sm:text-md mt-4 text-blue-200 px-4 lg:px-8">
+                  <p className="py-2">Would you like to use Fider without the hassle of hosting it yourself?</p>
+                  <p className="py-2">
+                    Become a monthly financial sponsor of our open source project and we&apos;ll host a fully managed Fider for instance for you so you
+                    don&apos;t have to worry about maintainance or updates.
+                  </p>
+                  <p className="py-2">We do&apos;t set a price. Support us with what you feel the software and service is worth.</p>
+                  <p className="py-2">Read the FAQ below for full details.</p>
+                </div>
+                <a
+                  href="https://login.fider.io/signup"
+                  className="flex items-center justify-center bg-indigo-800 hover:bg-indigo-700 p-8 text-md font-semibold text-gray-300 uppercase mt-8"
+                >
+                  <span>Sign up</span>
+                  <span className="font-medium text-gray-300 ml-2">➔</span>
+                </a>
+              </div>
             </div>
           </div>
+          <FAQ />
         </div>
-      </div>
-      <div className="w-full bg-gray-700">
-        <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-          <polygon className="text-gray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
-        </svg>
-      </div>
-    </section>
+        <div className="w-full bg-gray-700">
+          <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+            <polygon className="text-gray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+          </svg>
+        </div>
+      </section>
+    </>
   )
 }
